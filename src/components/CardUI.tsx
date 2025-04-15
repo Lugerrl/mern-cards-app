a7f7a800ec0be249b36a10a29f524912532b8776
Lucas Rolon <Lucas.rolon@outlook.com>
2025-04-14 22:53:59 -0400

    Add placeholder components for LoggedInName and CardUI

---
+++ src/components/CardUI.tsx
@@ -0,0 +1,14 @@
+function CardUI() {
+    return (
+      <div>
+        <input type="text" placeholder="Search card..." />
+        <button>Search</button>
+        <br />
+        <input type="text" placeholder="Add card..." />
+        <button>Add</button>
+      </div>
+    );
+  }
+  
+  export default CardUI;
+  
---
+++ src/components/LoggedInName.tsx
@@ -0,0 +1,11 @@
+function LoggedInName() {
+    return (
+      <div>
+        <span>Logged In As: Placeholder User</span><br />
+        <button>Log Out</button>
+      </div>
+    );
+  }
+  
+  export default LoggedInName;
+  
