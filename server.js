55a4273b89ca0c009794cf118193de94b2ad9e44
Lucas Rolon <Lucas.rolon@outlook.com>
2025-04-15 00:19:25 -0400

    commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
 require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const app = express();
 app.use(cors());
